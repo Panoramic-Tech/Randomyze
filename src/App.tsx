5f4e7c1576a1dc5ffa5e5f8c911fcb3b121ee9c0
Atul Bhatt <38177419+atulbhatt-system32@users.noreply.github.com>
2024-01-14 18:20:40 +0530

    shadcn setup and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Welcome to Know it Today</h1>
+    <div className="flex w-full h-100 min-h-screen flex-col space-y-4 p-8 justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Welcome to Know it Today</h1>
+      <div>
+        <Button>Randomyze</Button>
+      </div>
+    </div>
   );
 }
 
